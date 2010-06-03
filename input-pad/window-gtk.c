840aeeae7a54acb153a9afe0924aff72e902177b
fujiwarat <takao.fujiwara1@gmail.com>
2010-06-03 13:49:47 +0900

    Changed to LGPL.

--- input-pad/window-gtk.c
+++ input-pad/window-gtk.c
@@ -3,19 +3,20 @@
  * Copyright (C) 2010 Takao Fujiwara <takao.fujiwara1@gmail.com>
  * Copyright (C) 2010 Red Hat, Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301  USA
  */
 
 #ifdef HAVE_CONFIG_H
@@ -1619,21 +1620,22 @@ set_about (GtkWidget *widget)
     GtkAboutDialog *about_dlg = GTK_ABOUT_DIALOG (widget);
     gchar *license1, *license2, *license3, *license;
     license1 = _(""
-"This program is free software; you can redistribute it and/or modify "
-"it under the terms of the GNU General Public License as published by "
-"the Free Software Foundation; either version 2 of the License, or "
-"(at your option) any later version.");
+"This library is free software; you can redistribute it and/or "
+"modify it under the terms of the GNU Lesser General Public "
+"License as published by the Free Software Foundation; either "
+"version 2 of the License, or (at your option) any later version.");
 
     license2 = _(""
-"This program is distributed in the hope that it will be useful, "
+"This library is distributed in the hope that it will be useful, "
 "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
-"GNU General Public License for more details.");
+"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU "
+"Lesser General Public License for more details.");
 
     license3 = _(""
-"You should have received a copy of the GNU General Public License along "
-"with this program; if not, write to the Free Software Foundation, Inc., "
-"51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.");
+"You should have received a copy of the GNU Lesser General Public "
+"License along with this library; if not, write to the Free Software "
+"Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, "
+"MA  02110-1301  USA");
 
     license = g_strdup_printf ("%s\n\n%s\n\n%s",
                                license1, license2, license3);
