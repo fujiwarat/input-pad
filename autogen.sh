44bed6fe9861b72e4ff1bf75e67e83c73a83b54f
fujiwarat <takao.fujiwara1@gmail.com>
2013-12-03 15:20:30 +0900

    Revised CFLAGS in autogen.sh .

--- autogen.sh
+++ autogen.sh
@@ -9,6 +9,11 @@ aclocal --force || exit 1
 autoheader --force || exit 1
 automake --add-missing --copy --force || exit 1
 autoconf --force || exit 1
-export CFLAGS=${CFLAGS:-"-Wall -g -O0 -Wl,--no-undefined"}
-export CXXFLAGS="$CFLAGS"
-./configure "$@" || exit 1
+
+if `echo "$@" | grep -q CFLAGS > /dev/null` ; then
+    ./configure "$@" || exit 1
+else
+    CFLAGS=${CFLAGS-"-Wall -Wformat -Werror=format-security"}
+    CXXFLAGS="$CFLAGS"
+    ./configure "$@" CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" || exit 1
+fi
