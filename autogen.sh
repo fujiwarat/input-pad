ca5613a97e4eba569bc248a1620c105a31dbb55e
fujiwarat <takao.fujiwara1@gmail.com>
2025-03-21 23:37:07 +0900

    Migrate intltool to gettext

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,38 @@
 #!/bin/sh
-set -e
-set -x
+# Run this to generate all the initial makefiles, etc.
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
 
-libtoolize --automake --copy --force || exit 1
-mkdir -p m4
-intltoolize --copy --force || exit 1
-aclocal --force || exit 1
-autoheader --force || exit 1
-automake --add-missing --copy --force || exit 1
-autoconf --force || exit 1
+olddir=$(pwd)
 
-if `echo "$@" | grep -q CFLAGS > /dev/null` ; then
-    ./configure "$@" || exit 1
+cd "$srcdir"
+
+(test -f configure.ac) || {
+        echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
+        exit 1
+}
+
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+        echo "*** If you wish to pass any to it, please specify them on the" >&2
+        echo "*** '$0' command line." >&2
+        echo "" >&2
+fi
+
+autoreconf --verbose --force --install || exit 1
+
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
+
+        if [ "$1" = "--help" ]; then
+                exit 0
+        else
+                echo "Now type 'make' to compile $PKG_NAME" || exit 1
+        fi
 else
-    CFLAGS=${CFLAGS-"-Wall -Wformat -Werror=format-security"}
-    CXXFLAGS="$CFLAGS"
-    ./configure "$@" CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" || exit 1
+        echo "Skipping configure process."
 fi
